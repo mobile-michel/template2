17b95624bcda1a3007a9cd3ea361bcc84cbcafc2
Michel Maillard <info@maillard.li>
2023-04-16 18:10:54 +0200

    01-Installation

---
+++ eleventy.config.js
@@ -0,0 +1,11 @@
+const eleventySass = require("eleventy-sass");
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventySass);
+  eleventyConfig.addPassthroughCopy("./src/assets/");
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+  };
+};
